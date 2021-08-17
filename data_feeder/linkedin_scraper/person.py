5a488cf3a855b5cfd9911f1c7a2a11aa1db98bf4
alifgiant <alif.akbar@bukalapak.com>
2021-08-17 15:33:29 +0800

    dump data to generated yaml

--- data_feeder/linkedin_scraper/person.py
+++ data_feeder/linkedin_scraper/person.py
@@ -149,106 +149,106 @@ class Person(Scraper):
         #     "window.scrollTo(0, Math.ceil(document.body.scrollHeight/2));"
         # )
 
-        # get experience
-        driver.execute_script(
-            "window.scrollTo(0, Math.ceil(document.body.scrollHeight*3/5));"
-        )
+        # # get experience
+        # driver.execute_script(
+        #     "window.scrollTo(0, Math.ceil(document.body.scrollHeight*3/5));"
+        # )
 
-        ## Click SEE MORE
-        self._click_see_more_by_class_name("pv-experience-section__see-more")
+        # ## Click SEE MORE
+        # self._click_see_more_by_class_name("pv-experience-section__see-more")
 
-        try:
-            _ = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
-                EC.presence_of_element_located((By.ID, "experience-section"))
-            )
-            exp = driver.find_element_by_id("experience-section")
-        except:
-            exp = None
+        # try:
+        #     _ = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
+        #         EC.presence_of_element_located((By.ID, "experience-section"))
+        #     )
+        #     exp = driver.find_element_by_id("experience-section")
+        # except:
+        #     exp = None
 
-        if exp is not None:
-            for position in exp.find_elements_by_class_name("pv-position-entity"):
-                position_title = position.find_element_by_tag_name("h3").text.strip()
+        # if exp is not None:
+        #     for position in exp.find_elements_by_class_name("pv-position-entity"):
+        #         position_title = position.find_element_by_tag_name("h3").text.strip()
 
-                try:
-                    company = position.find_elements_by_tag_name("p")[1].text.strip()
-                    times = str(
-                        position.find_elements_by_tag_name("h4")[0]
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
-                    )
-                    from_date = " ".join(times.split(" ")[:2])
-                    to_date = " ".join(times.split(" ")[3:])
-                    duration = (
-                        position.find_elements_by_tag_name("h4")[1]
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
-                    )
-                    location = (
-                        position.find_elements_by_tag_name("h4")[2]
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
-                    )
-                except:
-                    company = None
-                    from_date, to_date, duration, location = (None, None, None, None)
+        #         try:
+        #             company = position.find_elements_by_tag_name("p")[1].text.strip()
+        #             times = str(
+        #                 position.find_elements_by_tag_name("h4")[0]
+        #                 .find_elements_by_tag_name("span")[1]
+        #                 .text.strip()
+        #             )
+        #             from_date = " ".join(times.split(" ")[:2])
+        #             to_date = " ".join(times.split(" ")[3:])
+        #             duration = (
+        #                 position.find_elements_by_tag_name("h4")[1]
+        #                 .find_elements_by_tag_name("span")[1]
+        #                 .text.strip()
+        #             )
+        #             location = (
+        #                 position.find_elements_by_tag_name("h4")[2]
+        #                 .find_elements_by_tag_name("span")[1]
+        #                 .text.strip()
+        #             )
+        #         except:
+        #             company = None
+        #             from_date, to_date, duration, location = (None, None, None, None)
 
-                experience = Experience(
-                    position_title=position_title,
-                    from_date=from_date,
-                    to_date=to_date,
-                    duration=duration,
-                    location=location,
-                )
-                experience.institution_name = company
-                self.add_experience(experience)
+        #         experience = Experience(
+        #             position_title=position_title,
+        #             from_date=from_date,
+        #             to_date=to_date,
+        #             duration=duration,
+        #             location=location,
+        #         )
+        #         experience.institution_name = company
+        #         self.add_experience(experience)
 
-        # get location
-        location = driver.find_element_by_class_name(f"{self.__TOP_CARD}--list-bullet")
-        location = location.find_element_by_tag_name("li").text
-        self.add_location(location)
+        # # get location
+        # location = driver.find_element_by_class_name(f"{self.__TOP_CARD}--list-bullet")
+        # location = location.find_element_by_tag_name("li").text
+        # self.add_location(location)
 
-        driver.execute_script(
-            "window.scrollTo(0, Math.ceil(document.body.scrollHeight/1.5));"
-        )
+        # driver.execute_script(
+        #     "window.scrollTo(0, Math.ceil(document.body.scrollHeight/1.5));"
+        # )
 
-        # get education
-        ## Click SEE MORE
-        self._click_see_more_by_class_name("pv-education-section__see-more")
-        try:
-            _ = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
-                EC.presence_of_element_located((By.ID, "education-section"))
-            )
-            edu = driver.find_element_by_id("education-section")
-        except:
-            edu = None
-        if edu:
-            for school in edu.find_elements_by_class_name(
-                "pv-profile-section__list-item"
-            ):
-                university = school.find_element_by_class_name(
-                    "pv-entity__school-name"
-                ).text.strip()
+        # # get education
+        # ## Click SEE MORE
+        # self._click_see_more_by_class_name("pv-education-section__see-more")
+        # try:
+        #     _ = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
+        #         EC.presence_of_element_located((By.ID, "education-section"))
+        #     )
+        #     edu = driver.find_element_by_id("education-section")
+        # except:
+        #     edu = None
+        # if edu:
+        #     for school in edu.find_elements_by_class_name(
+        #         "pv-profile-section__list-item"
+        #     ):
+        #         university = school.find_element_by_class_name(
+        #             "pv-entity__school-name"
+        #         ).text.strip()
 
-                try:
-                    degree = (
-                        school.find_element_by_class_name("pv-entity__degree-name")
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
-                    )
-                    times = (
-                        school.find_element_by_class_name("pv-entity__dates")
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
-                    )
-                    from_date, to_date = (times.split(" ")[0], times.split(" ")[2])
-                except:
-                    degree = None
-                    from_date, to_date = (None, None)
-                education = Education(
-                    from_date=from_date, to_date=to_date, degree=degree
-                )
-                education.institution_name = university
-                self.add_education(education)
+        #         try:
+        #             degree = (
+        #                 school.find_element_by_class_name("pv-entity__degree-name")
+        #                 .find_elements_by_tag_name("span")[1]
+        #                 .text.strip()
+        #             )
+        #             times = (
+        #                 school.find_element_by_class_name("pv-entity__dates")
+        #                 .find_elements_by_tag_name("span")[1]
+        #                 .text.strip()
+        #             )
+        #             from_date, to_date = (times.split(" ")[0], times.split(" ")[2])
+        #         except:
+        #             degree = None
+        #             from_date, to_date = (None, None)
+        #         education = Education(
+        #             from_date=from_date, to_date=to_date, degree=degree
+        #         )
+        #         education.institution_name = university
+        #         self.add_education(education)
 
         # get interest
         # try:
@@ -274,30 +274,30 @@ class Person(Scraper):
         # except:
         #     pass
 
-        # get accomplishment
-        try:
-            _ = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
-                EC.presence_of_element_located(
-                    (
-                        By.XPATH,
-                        "//*[@class='pv-profile-section pv-accomplishments-section artdeco-container-card artdeco-card ember-view']",
-                    )
-                )
-            )
-            acc = driver.find_element_by_xpath(
-                "//*[@class='pv-profile-section pv-accomplishments-section artdeco-container-card artdeco-card ember-view']"
-            )
-            for block in acc.find_elements_by_xpath(
-                "//div[@class='pv-accomplishments-block__content break-words']"
-            ):
-                category = block.find_element_by_tag_name("h3")
-                for title in block.find_element_by_tag_name(
-                    "ul"
-                ).find_elements_by_tag_name("li"):
-                    accomplishment = Accomplishment(category.text, title.text)
-                    self.add_accomplishment(accomplishment)
-        except:
-            pass
+        # # get accomplishment
+        # try:
+        #     _ = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
+        #         EC.presence_of_element_located(
+        #             (
+        #                 By.XPATH,
+        #                 "//*[@class='pv-profile-section pv-accomplishments-section artdeco-container-card artdeco-card ember-view']",
+        #             )
+        #         )
+        #     )
+        #     acc = driver.find_element_by_xpath(
+        #         "//*[@class='pv-profile-section pv-accomplishments-section artdeco-container-card artdeco-card ember-view']"
+        #     )
+        #     for block in acc.find_elements_by_xpath(
+        #         "//div[@class='pv-accomplishments-block__content break-words']"
+        #     ):
+        #         category = block.find_element_by_tag_name("h3")
+        #         for title in block.find_element_by_tag_name(
+        #             "ul"
+        #         ).find_elements_by_tag_name("li"):
+        #             accomplishment = Accomplishment(category.text, title.text)
+        #             self.add_accomplishment(accomplishment)
+        # except:
+        #     pass
 
         # get connections
         # try:
--- data_feeder/retrieve_data.py
+++ data_feeder/retrieve_data.py
@@ -4,29 +4,53 @@ from linkedin_scraper import Person, actions
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 
-# opts = Options()
-# opts.headless = True
-# driver = Chrome(options=opts)
+def setup_driver(email, password):
+    opts = Options()
+    opts.headless = True
+    driver = Chrome(options=opts)
+    actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
+    return driver
 
-email = "xxxx"
-password = "yyyy"
-# actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
-# person = Person("https://www.linkedin.com/in/auliadwio/", driver=driver)
-# person = Person("https://www.linkedin.com/in/muhammad-alif-akbar/", driver=driver)
+def read_generation_yaml(path, filename):
+    with open(f"{path}/{filename}", 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return {}
+
+def save_generation_yaml(path, filename, data):
+    with open(f"{path}_generated/{filename}", 'w') as stream:
+        try:
+            yaml.dump(data, stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+def crawl(member_data_path, filename):
+    print(f'========= processing: {filename} ========')
+    gen_data = read_generation_yaml(member_data_path, filename)
+    for idx, member in enumerate(gen_data['members']):
+        print(f"{idx+1}. {member['name']} : {member['linkedin']}")
+        
+        # crawling
+        person = Person(member['linkedin'], driver=driver, close_on_complete=False)
+        member['full_name'] = person.name
+        member['headline'] = person.headline
+        member['about'] = person.about
+        member['profile_image'] = person.profile_image
+        member['cover_image'] = person.cover_image
+    save_generation_yaml(member_data_path, filename, gen_data)
+    print(f'========= saved: {member_data_path}_generated/{filename} ========')
 
 if __name__ == '__main__':
+    email = "xxxx"
+    password = "yyyy"
+    driver = setup_driver(email, password)
+
     member_data_path = "./members_data"
     generation_file_name = []
     for dirpath, dnames, fnames in os.walk(member_data_path):
-        # print(f"{dirpath}, {dnames}, {fnames}")
         generation_file_name = fnames
     for filename in generation_file_name:
-        print(f'========= processing: {filename} ========')
-        gen_data = {}
-        with open(f"{member_data_path}/{filename}", 'r') as stream:
-            try:
-                gen_data = yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                print(exc)
-        for idx, member in enumerate(gen_data['members']):
-            print(f"{idx+1}. {member['name']} : {member['linkedin']}")
+        crawl(member_data_path, filename)
+    driver.quit()
